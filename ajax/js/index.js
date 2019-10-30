0df5844274c8cf3f97e984a35ee1d87a5e675e04
2jmalvarez <30739456+2jmalvarez@users.noreply.github.com>
2019-10-30 16:49:21 -0300

    Add files via upload

---
+++ ajax/js/index.js
@@ -0,0 +1,40 @@
+window.addEventListener("load",function () {
+    let boton = this.document.getElementById("btn")
+
+    boton.addEventListener("click",traerTexto)
+});
+
+
+function traerTexto(){
+
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+
+        if(xhr.readyState == 4){
+            document.getElementById("loader").innerHTML = ""
+            if(xhr.status == 200){
+                let info = document.getElementById("info");
+                info.innerText = xhr.responseText;
+            }
+            else {
+                console.log(xhr.status + " : "+ xhr.statusText);
+                
+            }
+
+
+        }else{
+            let span = document.getElementById("loader")
+            span.innerHTML = "<img src='./js/images/safe_image.gif'></img>";
+        }
+
+
+        
+    }
+
+    xhr.open("GET","./documento.txt",true);
+
+    xhr.send();
+
+}
